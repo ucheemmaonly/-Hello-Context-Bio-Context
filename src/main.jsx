73af14134759a2910e8ffb858a6f68ec85aa2f3a
Uche Emmanuel <ucheemmanuel20222@gmail.com>
2026-09-03 14:21:33 +0100

    chores

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx';
+import { HelloProvider } from './context/HelloContext.jsx';
+import { BioProvider } from './context/BioContext.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <HelloProvider>
+      <BioProvider>
+        <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+      </BioProvider>
+      
+    </HelloProvider>
+     
+  </AuthProvider>
+ 
+);
